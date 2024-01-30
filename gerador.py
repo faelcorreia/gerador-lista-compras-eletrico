f98d1943bcebcf0ffc52360f61a4cbc7a7310e4d
Rafael Correia <rafaelnominato@gmail.com>
2024-01-30 19:36:03 -0300

    Adiciona suportes de placas

--- gerador.py
+++ gerador.py
@@ -26,6 +26,8 @@ for key in data.keys():
         else:
             item = f'{i["descricao"]} ({i["cor"]})'
         set_or_increase(item, i["quantidade"])
+        if "placa" in i["descricao"]:
+            set_or_increase(f'suporte {i["descricao"]}', i["quantidade"])
         for slot in i["slots"]:
             item = f'{slot} ({i["cor"]})'
             set_or_increase(item, 1)
